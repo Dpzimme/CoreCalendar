8a192af7d7069f9cc8fa616e4543a22d6f5fb9bf
Dan Zimmerman <danzimmerman@sbcglobal.net>
2020-10-13 16:43:40 -0700

    removes extra folders and files

--- storage/CalendarE2.WebAppOneBack/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CalendarE2.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
